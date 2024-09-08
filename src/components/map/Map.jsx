c20ab1a348b4cc638f1884fecb254627af35dbeb
yamineHjr <yasmine.alnajar@gmail.com>
2024-09-08 22:25:34 +0300

    fix map exception

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -14,9 +14,9 @@ function Map({items}){
       zoom={7} 
       scrollWheelZoom={false} 
       className='map'>
-    <TileLayer
-      // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <TileLayer style={{ zIndex: '999'}}
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     {items.map((item) => (
         <Pin item={item} key={item.id} />
