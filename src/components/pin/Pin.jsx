22a79bdb437d1aa2c80418b4802b81f0a81aebab
yamineHjr <yasmine.alnajar@gmail.com>
2024-09-08 22:42:54 +0300

    update marker icon

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
 
 function Pin({ item }) {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    // <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
       <Popup>
         <div className="popupContainer">
-          <img src={item.img} alt="" />
+          <img src={item.images[0]} alt="" />
           <div className="textContainer">
             <Link to={`/${item.id}`}>{item.title}</Link>
             <span>{item.bedroom} bedroom</span>
@@ -17,6 +20,7 @@ function Pin({ item }) {
         </div>
       </Popup>
     </Marker>
+    // <Marker position={[lat, lng]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} />
   );
 }
 
